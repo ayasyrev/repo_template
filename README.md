f91386e22488e5ee48307e4c9c703fd67d29c83f
ayasyrev <a.yasyrev@gmail.com>
2022-09-09 13:13:31 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # repo_template
 template for repo
+
+Source at src
---
+++ src/repo_template/app.py
@@ -0,0 +1,6 @@
+from .version import __version__
+
+
+def dummy_func():
+    """Dummy func. Just print version."""
+    print(f"Repo temlate. Version: {__version__}")
